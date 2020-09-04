77ac41f9ee8a83f528e19f0159e409dd17985fc7
Ziyad Zulfikar <56788667+ziyadzulfikar@users.noreply.github.com>
2020-09-04 16:24:11 +0530

    Updates

---
+++ precache-manifest.38a5ad98b00b6576ff7cf334650d8050.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d1339674750e2d7f0b076757d405770",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf0fad4ad29d02737b4d",
+    "url": "/static/css/main.76103688.chunk.css"
+  },
+  {
+    "revision": "0e7da6bdf7fe0d7f0a18",
+    "url": "/static/js/2.2c37138d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2c37138d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf0fad4ad29d02737b4d",
+    "url": "/static/js/main.32ce7258.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "50899514d78fad6d44f29ac06d8a9dbd",
+    "url": "/static/media/todo.50899514.jpg"
+  }
+]);
